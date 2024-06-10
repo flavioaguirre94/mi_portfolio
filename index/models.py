c99a3da4ceb80fb609a03ef07cc27cf35eb00bf4
flavioaguirre94 <flavio.j.aguirre@gmail.com>
2024-06-10 15:47:34 -0300

    Instalacion y seteo básico Django 1

---
+++ index/models.py
@@ -0,0 +1,8 @@
+# from django.db import models
+
+# class Proyectos(models.Model):
+#     nombre_proyecto = models.CharField(max_length=100)
+#     descripcion_proyecto = models.TextField()
+
+#     def __str__(self):
+#         return self.nombre_proyecto
