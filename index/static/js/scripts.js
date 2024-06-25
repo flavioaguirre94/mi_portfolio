c350ca835e264e947fdc021cc63bcc01590237e4
flavioaguirre94 <flavio.j.aguirre@gmail.com>
2024-06-25 11:57:04 -0300

    inicio modificacion 'Proyectos' 1.0

--- index/static/js/scripts.js
+++ index/static/js/scripts.js
@@ -1,11 +1,4 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+// Scripts 
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -54,7 +47,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
 // Inicializar ScrollReveal
 ScrollReveal().reveal('.reveal-top', {
-    distance: '15px',
+    distance: '25px',
     duration: 1000,
     easing: 'ease-out',
     origin: 'top',
